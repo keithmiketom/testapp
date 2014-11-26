07e2cba801d5223d516e975b27f1fe7eb3fb3890
Keith Michael Gregg <keithmichaelgregg@Keiths-MacBook-Pro.local>
2014-11-26 16:38:39 +0000

    removed welcome controller, added site controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
+  get 'site/index'
+
+  get 'site/about'
+
+  get 'site/learn'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'welcome#index'
+   root 'site#index'
    
-   resources :welcome
-   
-   get 'welcome/about' => 'welcome#about', as: :about
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
