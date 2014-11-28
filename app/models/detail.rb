d59e5dc6524d0929d7432b932370bd4c956b0800
Keith Michael Gregg <keithmichaelgregg@Keiths-MacBook-Pro.local>
2014-11-28 17:10:51 +0000

    added more styling, finished form collections. Next to do is devise

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -5,9 +5,9 @@ class Detail < ActiveRecord::Base
   
   PROFESSIONS = [ "Art Teacher", "Other Teacher", "IT Professional", "Student", "Other"]
   
-  TEACHLENGTH = [ "0-5 years (NQT)", "5-10 years", "10-15 years", "15-20 years", "20-25 years", "25 years +"]
+  TEACHLENGTH = [ "n/a", "0-5 years (NQT)", "5-10 years", "10-15 years", "15-20 years", "20-25 years", "25 years +"]
   
   LOCATION = ["Dublin", "Leinster", "Munster", "Connaught", "Ulster", "Northern Ireland", "UK", "Australia", "USA", "Canada", "Other"]
   
-  PROFICIENCY =[ "1 - Basic Understanding", "2 - General Understanding", "3 - Ablility to use program like Photoshop", "4 - Some Understanding of Code", "5 - Advanced Understanding"]
+  PROFICIENCY =[ "select", "1 - Basic Understanding", "2 - General Understanding", "3 - Ablility to use program like Photoshop", "4 - Some Understanding of Code", "5 - Advanced Understanding"]
 end
