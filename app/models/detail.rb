2f18ddcc2f47a99cefa489b90f51fe85cf3f4828
Keith Michael Gregg <keithmichaelgregg@Keiths-MacBook-Pro.local>
2014-11-28 14:03:50 +0000

    finished detail form

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -8,4 +8,6 @@ class Detail < ActiveRecord::Base
   TEACHLENGTH = [ "0-5 years (NQT)", "5-10 years", "10-15 years", "15-20 years", "20-25 years", "25 years +"]
   
   LOCATION = ["Dublin", "Leinster", "Munster", "Connaught", "Ulster", "Northern Ireland", "UK", "Australia", "USA", "Canada", "Other"]
+  
+  PROFICIENCY =[ "1 - Basic Understanding", "2 - General Understanding", "3 - Ablility to use program like Photoshop", "4 - Some Understanding of Code", "5 - Advanced Understanding"]
 end
