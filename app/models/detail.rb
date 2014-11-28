5793900ba12a936fab97ac9c3cf2b158033799eb
Keith Michael Gregg <keithmichaelgregg@Keiths-MacBook-Pro.local>
2014-11-28 13:28:13 +0000

    updated detail form

--- app/models/detail.rb
+++ app/models/detail.rb
@@ -1,6 +1,5 @@
 class Detail < ActiveRecord::Base
   
-  attr_accessible :name, :age, :email, :profession, :lenghtteach, :ictlevel, :research, :location
   
   validates_uniqueness_of :email
   
@@ -8,5 +7,5 @@ class Detail < ActiveRecord::Base
   
   TEACHLENGTH = [ "0-5 years (NQT)", "5-10 years", "10-15 years", "15-20 years", "20-25 years", "25 years +"]
   
-  LOCATION = ["Dublin South", "Dublin North", "Leinster", "Munster", "Connaught", "Ulster", "Northern Ireland", "UK", "Australia", "USA", "Canada", "Other"]
+  LOCATION = ["Dublin", "Leinster", "Munster", "Connaught", "Ulster", "Northern Ireland", "UK", "Australia", "USA", "Canada", "Other"]
 end
